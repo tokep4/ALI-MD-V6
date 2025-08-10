85cefdb14068b711f3a533292f1434e0207b364c
ali-lasharivj <queensapna26@gmail.com>
2025-08-10 23:44:30 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -17,69 +17,11 @@ const {
 // List of audio URLs
 const audioUrls = [
     "https://files.catbox.moe/hpwsi2.mp3",
-    "https://files.catbox.moe/xci982.mp3",
-    "https://files.catbox.moe/utbujd.mp3",
-    "https://files.catbox.moe/w2j17k.m4a",
-    "https://files.catbox.moe/851skv.m4a",
-    "https://files.catbox.moe/qnhtbu.m4a",
-    "https://files.catbox.moe/lb0x7w.mp3",
-    "https://files.catbox.moe/efmcxm.mp3",
-    "https://files.catbox.moe/gco5bq.mp3",
-    "https://files.catbox.moe/26oeeh.mp3",
-    "https://files.catbox.moe/a1sh4u.mp3",
-    "https://files.catbox.moe/vuuvwn.m4a",
-    "https://files.catbox.moe/wx8q6h.mp3",
-    "https://files.catbox.moe/uj8fps.m4a",
-    "https://files.catbox.moe/dc88bx.m4a",
-    "https://files.catbox.moe/tn32z0.m4a",
-    "https://files.catbox.moe/9fm6gi.mp3",
-    "https://files.catbox.moe/9h8i2a.mp3",
-    "https://files.catbox.moe/5pm55z.mp3",
     "https://files.catbox.moe/zjk77k.mp3",
     "https://files.catbox.moe/fe5lem.m4a",
     "https://files.catbox.moe/4b1ohl.mp3"
 ];
 
-// List of video URLs
-const videoUrls = [
-    "https://i.imgur.com/Zuun5CJ.mp4",
-    "https://i.imgur.com/tz9u2RC.mp4",
-    "https://i.imgur.com/W7dm6hG.mp4",
-    "https://i.imgur.com/vElB6hY.mp4",
-    "https://i.imgur.com/KEs4JGe.mp4",
-    "https://i.imgur.com/ry0z5iY.mp4",
-    "https://i.imgur.com/gUVzqp0.mp4",
-    "https://i.imgur.com/5zXuU0f.mp4",
-    "https://i.imgur.com/7tYyJst.mp4",
-    "https://i.imgur.com/1sf08VI.mp4",
-    "https://i.imgur.com/b82uXEY.mp4",
-    "https://i.imgur.com/ZmUHE9n.mp4",
-    "https://i.imgur.com/V6wOAMg.mp4",
-    "https://i.imgur.com/LBeULsl.mp4",
-    "https://i.imgur.com/lvKo3PN.mp4",
-    "https://i.imgur.com/HMqqdpN.mp4",
-    "https://i.imgur.com/xm4j3yT.mp4",
-    "https://i.imgur.com/oJTzrh0.mp4",
-    "https://i.imgur.com/DSOZOx3.mp4",
-    "https://i.imgur.com/GNm3TCN.mp4",
-    "https://i.imgur.com/WVYlxDb.mp4",
-    "https://i.imgur.com/xPbhwjz.mp4",
-    "https://i.imgur.com/VDUNXrp.mp4",
-    "https://i.imgur.com/wynK50w.mp4",
-    "https://i.imgur.com/CF79jVv.mp4",
-    "https://i.imgur.com/CF79jVv.mp4",
-    "https://i.imgur.com/CF79jVv.mp4",
-    "https://i.imgur.com/Ob7hzMz.mp4",
-    "https://i.imgur.com/etEV3om.mp4",
-    "https://i.imgur.com/OLqupmU.mp4",
-    "https://i.imgur.com/OLqupmU.mp4",
-    "https://i.imgur.com/imDK97l.mp4",
-    "https://i.imgur.com/pNArg0x.mp4",
-    "https://i.imgur.com/pNArg0x.mp4",
-    "https://i.imgur.com/wIEwUS2.mp4",
-    "https://i.imgur.com/upNLbX8.mp4",
-    "https://i.imgur.com/AlKaIdq.mp4"
-];
 
 // List of random facts and quotes
 const factsAndQuotes = [
@@ -99,11 +41,6 @@ function getRandomAudioUrl() {
     return audioUrls[randomIndex];
 }
 
-// Function to get a random video URL
-function getRandomVideoUrl() {
-    const randomIndex = Math.floor(Math.random() * videoUrls.length);
-    return videoUrls[randomIndex];
-}
 
 // Function to get a random fact/quote
 function getRandomFactOrQuote() {
@@ -156,22 +93,13 @@ router.get('/', async (req, res) => {
 
                     // Send session data first
                     let sessionMessage = await Pair_Code_By_Ibrahim_Adams.sendMessage(Pair_Code_By_Ibrahim_Adams.user.id, {
-                        text: 'ALPHA;;;' + b64data
+                        text: 'ALI-MD~' + b64data
                     });
 
-                    // Get a random fact/quote
-                    let randomFactOrQuote = getRandomFactOrQuote();
-                    let randomVideoUrl = getRandomVideoUrl(); // Get a random video URL
-
-                    // Send the video with caption
-                    await Pair_Code_By_Ibrahim_Adams.sendMessage(Pair_Code_By_Ibrahim_Adams.user.id, { 
-                        video: { url: randomVideoUrl },
-                        caption: randomFactOrQuote 
-                    });
-
+                   
                     // Send the random audio URL as a voice note
                     const randomAudioUrl = getRandomAudioUrl(); // Get a random audio URL
-                    await Pair_Code_By_Ibrahim_Adams.sendMessage(Pair_Code_By_Ibrahim_Adams.user.id, { 
+                    await Pair_Code_By_Ibrahim_Adams.sendMessage(Pair_Code_By_Ibrahim_Adams.user.id, {
                         audio: { url: randomAudioUrl },
                         mimetype: 'audio/mp4', // MIME type for voice notes
                         ptt: true,
@@ -180,10 +108,10 @@ router.get('/', async (req, res) => {
                         contextInfo: {
                             mentionedJid: [Pair_Code_By_Ibrahim_Adams.user.id], // Mention the sender in the audio message
                             externalAdReply: {
-                                title: 'Thanks for choosing 𝗞𝗲𝗶𝘁𝗵 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 happy deployment 💜',
-                                body: 'Regards Keithkeizzah',
-                                thumbnailUrl: 'https://i.imgur.com/vTs9acV.jpeg',
-                                sourceUrl: 'https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47',
+                                title: '𝐓𝐇𝐀𝐍𝐊𝐒 𝐅𝐎𝐑 𝐂𝐇𝐎𝐎𝐒𝐈𝐍𝐆 𝐀𝐋𝐈 𝐌𝐃',
+                                body: '𝐑𝐄𝐆𝐀𝐑𝐃𝐒 𝐀𝐋𝐈 𝐈𝐍𝐗𝐈𝐃𝐄',
+                                thumbnailUrl: 'https://files.catbox.moe/6ku0eo.jpg',
+                                sourceUrl: 'https://whatsapp.com/channel/0029VaoRxGmJpe8lgCqT1T2h',
                                 mediaType: 1,
                                 renderLargerThumbnail: true,
                             },
